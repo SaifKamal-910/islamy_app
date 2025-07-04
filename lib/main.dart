ca274600464094837d276be110abf8cacb8cc6bd
Seif Kamal <seifkamal389@gmail.com>
2025-07-04 15:14:05 +0300

    initialize project

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:islami_application/home/home_screen.dart';
+
+class IslamiApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {HomeScreen.routeName: (context) => HomeScreen()},
+    );
+  }
+}
+
+void main() {
+  runApp(IslamiApp());
+}
